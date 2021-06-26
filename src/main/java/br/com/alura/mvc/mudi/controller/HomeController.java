cae2e32304f13a8027a0df4e38034b5b585ed2be
Marcio Tomaz <marciotmaz@gmail.com>
2021-06-26 12:15:57 -0300

    feat: create mysql database and connect the application

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,6 +3,10 @@ package br.com.alura.mvc.mudi.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +16,15 @@ import br.com.alura.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("home")
 	public String home(Model mv) {
 		
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Yugioh! Card");
-		pedido.setUrlImagem("https://awesomecardgames.com/wp-content/uploads/2019/10/Dark-Magician-Girl-Art-1.jpg");
-		pedido.setUrlProduto("https://www.duelshop.com.br/yu-gi-oh/11153-borrelsword-dragon-cyho-en034-secret-rare.html");
-		pedido.setDescricao("Descricao toper");
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
 		mv.addAttribute("pedidos", pedidos);
 		
 		return "home";
--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -3,12 +3,22 @@ package br.com.alura.mvc.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+
+@Entity
 public class Pedido {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
